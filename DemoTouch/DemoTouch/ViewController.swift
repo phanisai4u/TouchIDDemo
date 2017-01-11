5d9ea7a0a83122281ff19830c016706d14fb180f
Phanisai <phanisai4u@gmail.com>
2017-01-11 15:24:21 +0530

    Add files via upload

---
+++ DemoTouch/DemoTouch/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  DemoTouch
+//
+//  Created by Phani on 10/01/17.
+//  Copyright © 2017 Phani. All rights reserved.
+//
+
+import UIKit
+import LocalAuthentication
+
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func validateMytouchID(_ sender: Any) {
+        
+        authenticateUser()
+    }
+    
+    
+    func authenticateUser() {
+        let context = LAContext()
+        context.isCredentialSet(.applicationPassword)
+        var error: NSError?
+        
+        if context.canEvaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, error: &error) {
+            let reason = "Identify yourself!"
+            
+            context.evaluatePolicy(.deviceOwnerAuthenticationWithBiometrics, localizedReason: reason) {
+                [unowned self] success, authenticationError in
+                
+                DispatchQueue.main.async {
+                    if success { 
+                        let data = context.evaluatedPolicyDomainState
+                        print(data?.base64EncodedString())
+                    } else {
+                        let ac = UIAlertController(title: "Authentication failed", message: "Sorry!", preferredStyle: .alert)
+                        ac.addAction(UIAlertAction(title: "OK", style: .default))
+                        self.present(ac, animated: true)
+                    }
+                }
+            }
+        } else {
+            let ac = UIAlertController(title: "Touch ID not available", message: "Your device is not configured for Touch ID.", preferredStyle: .alert)
+            ac.addAction(UIAlertAction(title: "OK", style: .default))
+            present(ac, animated: true)
+        }
+    }
+    
+    
+    
+}
+
